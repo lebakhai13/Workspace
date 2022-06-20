a38fc0055b7cf81532e69c305eb903edc159650c
lebakhai13 <lebakhai1312@gmail.com>
2022-06-20 09:48:13 +0700

    update fordel after 6 month no code

---
+++ JS/learn/lesson_1/assets/js/main.js
@@ -0,0 +1,31 @@
+// 1.length
+// console.log(fullName.length);
+
+// 2.Find index 
+// console.log(fullName.search('Bá'));
+
+// 3.cut string
+// console.log(fullName.slice(-7,-5));
+
+//4.Replace
+// console.log(fullName.replace(/Bá Khải/g,'Tiến Khá'));
+
+//5.Convert to upper case
+// console.log(fullName.toUpperCase());
+
+//6.Convert to lower case
+// console.log(fullName.toLowerCase());
+
+//7.Trim
+// console.log(fullName.trim());
+
+//8.Split
+// var languages = 'vietnamese, english, japanese, russia,chinese';
+// console.log(languages.split(', '));
+
+//9.Get a character by index
+// console.log(fullName.charAt(1));
+// console.log(fullName[0]);
+
+console.log(languages[0]);
+
---
+++ JS/learn/lesson_1/assets/js/var.js
@@ -0,0 +1,15 @@
+var firstName = 'Lê';
+var middleName = 'Bá';
+var lastName = 'Khải';
+var fullName = firstName + ' ' + middleName + ' ' + lastName ; //Lê Bá Khải
+var myAge = 13;
+var myNaN = 13 / ':))';
+var myNumber = 5.3589640;
+var languages = [
+    'Javascipt',
+    'PHP',
+    'Java',
+    'C',
+    'C+',
+    'Python',
+];
